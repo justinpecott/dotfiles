58eb9b6ea3ee69761d37ba0f33ca4840c51a78b6
Justin Pecott <justin@pecott.net>
2020-12-29 11:28:31 -0800

    add gems to path

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,4 @@ alias mv="mv -i"
 alias which="type -a"
 alias ng="npm ls -g --depth=0"
 alias nl="npm ls --depth=0"
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ruby/bin" "/usr/local/lib/ruby/gems/2.7.0/bin" $fish_user_paths
