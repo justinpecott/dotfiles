80682f03897dbbed96b4b86511997ccd8bb2f619
Justin Pecott <justin@pecott.net>
2022-02-01 07:02:26 +0000

    Nuke the github token

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ alias ng="npm ls -g --depth=0"
 alias nl="npm ls --depth=0"
 set -gx PATH $HOME/bin $PATH
 eval (/opt/homebrew/bin/brew shellenv)
-set -gx HOMEBREW_GITHUB_API_TOKEN "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"
+set -gx HOMEBREW_GITHUB_API_TOKEN "SOME_TOKEN"
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
