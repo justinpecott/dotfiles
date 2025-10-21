5225a6abc9dc6508d8fa39b960baf5ff06af06ae
Justin Pecott <justin@pecott.net>
2025-10-21 13:35:34 -0700

    Add electron detection scripts.

---
+++ bin/detect-electron-apps-by-version.sh
@@ -0,0 +1,26 @@
+# Detect affected Electron versions
+mdfind "kMDItemFSName == '*.app'" | sort --ignore-case | while read -r app; do
+  appName=$(basename "$app")
+  electronFrameworkInfo="$app/Contents/Frameworks/Electron Framework.framework/Resources/Info.plist"
+  if [[ -f "$electronFrameworkInfo" ]]; then
+    electronVersion=$(plutil -extract CFBundleVersion raw "$electronFrameworkInfo")
+      if [[ $? -eq 1 || -z "$electronVersion" ]]; then
+      echo "⚠️  $appName (No Electron version)"
+    else
+      IFS='.' read -r major minor patch <<< "$electronVersion"
+          
+      if [[ $major -gt 39 ]] || \
+        [[ $major -eq 39 && $minor -ge 0 ]] || \
+        [[ $major -eq 38 && $minor -gt 2 ]] || \
+        [[ $major -eq 38 && $minor -eq 2 && $patch -ge 0 ]] || \
+        [[ $major -eq 37 && $minor -gt 6 ]] || \
+        [[ $major -eq 37 && $minor -eq 6 && $patch -ge 0 ]] || \
+        [[ $major -eq 36 && $minor -gt 9 ]] || \
+        [[ $major -eq 36 && $minor -eq 9 && $patch -ge 2 ]]; then
+        echo "✅ $appName ($electronVersion)"
+      else
+        echo "❌️ $appName ($electronVersion)"
+      fi
+    fi
+  fi
+done
---
+++ bin/detect-electron-apps-using-cornermask.sh
@@ -0,0 +1,24 @@
+# Directly detect Electron apps using the _cornerMask override - thanks avarayr!
+mdfind "kMDItemFSName == '*.app'" | sort --ignore-case | while read -r app; do
+  electronFiles=$(find "$app" -name "Electron Framework" -type f 2>/dev/null)
+  
+  if [[ -n "$electronFiles" ]]; then
+    appName=$(basename "$app")
+    
+    while IFS= read -r filename; do
+      if [[ -f "$filename" ]]; then
+        ev=$(grep -aoE 'Chrome/.*Electron/[0-9]+(\.[0-9]+){1,3}' -- "$filename" 2>/dev/null | head -n1 | sed -E 's/.*Electron\/([0-9]+(\.[0-9]+){1,3}).*/\1/')
+        [ -z "$ev" ] && ev=$(grep -aoE 'Electron/[0-9]+(\.[0-9]+){1,3}' -- "$filename" 2>/dev/null | head -n1 | sed -E 's/.*Electron\/([0-9]+(\.[0-9]+){1,3}).*/\1/')
+        
+          relativePath="${filename#"$app/"}"
+        
+        if grep -aqF "_cornerMask" -- "$filename" 2>/dev/null; then
+          echo "❌ $appName (Electron ${ev:-unknown}) - $relativePath"
+        else
+          echo "✅ $appName (Electron ${ev:-unknown}) - $relativePath"
+        fi
+        break
+      fi
+    done <<< "$electronFiles"
+  fi
+done
